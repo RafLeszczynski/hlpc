b5b5a7ead743743a9c80e8eb2851a1dac5a7b4fe
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-11-04 20:05:03 +0100

    secure api endpoints and add login to frontend based on auth0 react examples

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const path = require('path');
 const express = require('express');
 const log = require('pino')();
 const bodyParser = require('body-parser');
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+const cors = require('cors');
 const tokenService = require('./server/lib/token-service');
 const errorLoggerMiddleware = require('./server/middleware/error-logger');
 const errorHandlerMiddleware = require('./server/middleware/error-handler');
@@ -14,17 +18,39 @@ const { getLoginPageDataMiddleware, updateLoginPageDataMiddleware } = require('.
 const app = express();
 const port = process.env.PORT_SERVER || 3001;
 
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on
+  // the kid in the header and the singing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+const checkScopes = jwtAuthz([process.env.AUTH0_HLPC_EDIT_SCOPE]);
+
 app.use(express.static(path.resolve(__dirname, './build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
-// TODO: secure end points
-app.get('/api/login-page-data', getLoginPageDataMiddleware);
-app.post('/api/login-page-data', updateLoginPageDataMiddleware);
+app.get('/api/login-page-data', checkJwt, checkScopes, getLoginPageDataMiddleware);
+app.post('/api/login-page-data', checkJwt, checkScopes, updateLoginPageDataMiddleware);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build/index.html'));
-});
+// For the purpose of easier MVP deployment on Heroku (free instance => 1 web dyno)
+// I decided to host from single app
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './build/index.html'));
+  });
+}
 
 app.use(errorLoggerMiddleware);
 app.use(errorHandlerMiddleware);
