509fa30630beb3ad3ed8a7fd38b8b58d4608302c
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-11-04 14:57:02 +0100

    add hello world react app

--- index.js
+++ index.js
@@ -2,23 +2,29 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load(); // eslint-disable-line global-require
 }
 
+const path = require('path');
 const express = require('express');
 const log = require('pino')();
 const bodyParser = require('body-parser');
-const tokenService = require('./lib/token-service');
-const errorLoggerMiddleware = require('./middleware/error-logger');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-const { getLoginPageDataMiddleware, updateLoginPageDataMiddleware } = require('./middleware/api');
+const tokenService = require('./server/lib/token-service');
+const errorLoggerMiddleware = require('./server/middleware/error-logger');
+const errorHandlerMiddleware = require('./server/middleware/error-handler');
+const { getLoginPageDataMiddleware, updateLoginPageDataMiddleware } = require('./server/middleware/api');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT_SERVER || 3001;
 
+app.use(express.static(path.resolve(__dirname, './build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // TODO: secure end points
 app.get('/api/login-page-data', getLoginPageDataMiddleware);
 app.post('/api/login-page-data', updateLoginPageDataMiddleware);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './build/index.html'));
+});
 
 app.use(errorLoggerMiddleware);
 app.use(errorHandlerMiddleware);
