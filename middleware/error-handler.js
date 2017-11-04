0e85e167d89335e739ba47bf6e2da7fdd1be883b
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-11-04 13:50:19 +0100

    fix linting errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,6 @@
 const apiResponseBuilder = require('../lib/api-response-builder');
 
-module.exports = (err, req, res, next) => {
+module.exports = (err, req, res, next) => { // eslint-disable-line no-unused-vars
   res
     .status(err.httpCode || 500)
     .json(apiResponseBuilder.createErrorResponse(err));
